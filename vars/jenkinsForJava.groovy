9911b509b60b6bf647bc92ffede4e5cd538429e7
Suman Goel <42088954+suman2826@users.noreply.github.com>
2021-10-02 18:32:46 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -8,31 +8,18 @@ def call(String repoUrl) {
        stages {
            stage("Tools initialization") {
                steps {
-                   sh "mvn --version"
-                   sh "java -version"
+                   
+                   echo "suman"
                }
            }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
+           
+           
            stage("Running Testcase") {
               steps {
-                   sh "mvn test"
+                   echo "completed"
                }
            }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
+           
        }
    }
 }
