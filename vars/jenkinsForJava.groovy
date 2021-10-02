1c8921a54b482b943a2e754c3a62d9d1ecab24b1
Suman Goel <42088954+suman2826@users.noreply.github.com>
2021-10-02 19:17:03 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -5,7 +5,6 @@ def call() {
        stages {
            stage("Tools initialization") {
                steps {
-                   sh 'echo "A one line step"'
                    echo "suman"
                }
            }
