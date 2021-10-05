143751929595fc66fec4648ba2156ff2f02a157e
Suman Goel <42088954+suman2826@users.noreply.github.com>
2021-10-05 09:24:32 +0530

    Update loadUtility.groovy

--- vars/loadUtility.groovy
+++ vars/loadUtility.groovy
@@ -1,7 +1,7 @@
 def call() {
    echo "inside loadutility"
    def rootDir = pwd()
-   def obj = load "${rootDir}@script/utilities.Groovy "
+   def obj = load "${rootDir}@script/utilities.groovy"
 //   def obj = load "vars/utilities.groovy"
    echo "here"
  
